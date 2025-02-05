c05b89dfeaadb5f79b1e870bf6d78143e9b8c179
scorcism <abhishekpathak1720@gmail.com>
2025-02-05 20:14:00 +0530

    Update vite config, add env example

--- cohesion-web/vite.config.ts
+++ cohesion-web/vite.config.ts
@@ -1,12 +1,20 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
